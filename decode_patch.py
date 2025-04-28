39ad3d0b99645ff3a227163a7204de91a9f5a6f4
Leon Lee <leonlee20031219@gmail.com>
2025-04-29 00:22:46 +1000

    added script to create prompt file from .json files, modified decode_patch.py to accept command line arguments

--- decode_patch.py
+++ decode_patch.py
@@ -1,9 +1,20 @@
-import json
+"""
+Usage: python save_patch.py instance.json output.txt patch_key
+patch_key must be one of: patch, test_patch, problem_statement
+"""
 
-with open('prs/17180/17180.json') as f:
+import json
+import sys
+
+input_json = sys.argv[1]
+output_path = sys.argv[2]
+patch_key = sys.argv[3]  # Added this: "patch" or "test_patch"
+
+with open(input_json) as f:
     task = json.load(f)
 
-patch = task["patch"].replace('\\n', '\n')  # IMPORTANT: decode \n to real newlines
+# Read the right field: patch or test_patch
+patch_content = task[patch_key].replace('\\n', '\n')
 
-with open('fix.patch', 'w') as f:
-    f.write(patch)
+with open(output_path, 'w') as f:
+    f.write(patch_content)
