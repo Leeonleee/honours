71431cc3a24337fa750a5f92d95dc145bec65f27
Your Name <you@example.com>
2025-08-12 02:19:36 +0000

    Ran full duckdb benchmark on o3, 4o, gpt-5, gemini 2.5 pro

--- scripts/collection/run_get_tasks_pipeline.sh
+++ scripts/collection/run_get_tasks_pipeline.sh
@@ -5,6 +5,6 @@
 # * Declare GITHUB_TOKENS=token1,token2,token3...
 
 python3 get_tasks_pipeline.py \
-    --repos 'ydb-platform/ydb' \
+    --repos 'ClickHouse/ClickHouse' \
     --path_prs '/home/ec2-user/Documents/university/honours/scripts/collection/output/prs' \
     --path_tasks '/home/ec2-user/Documents/university/honours/scripts/collection/output/tasks'
